37a4a3d41a752498b832e8682641be82b1b7ee64
Anurag Rawat <anuragrawat0608@gmail.com>
2026-02-10 23:02:22 +0530

    Fix: convert client from broken submodule to normal folder

---
+++ client/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
+const page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+       <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default page
